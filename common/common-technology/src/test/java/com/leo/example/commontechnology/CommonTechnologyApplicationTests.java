a2236c197e21a9bd1a49016d565d5fd172ababf4
shipleyleo <shipleyleo@163.com>
2022-04-08 15:15:17 +0800

    【ADD】新增common目录，存放common-utils、common-technology模块；

---
+++ common/common-technology/src/test/java/com/leo/example/commontechnology/CommonTechnologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.example.commontechnology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonTechnologyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
